a263b98d6242ec4d5e4c2dcdb495a5caa7cf2782
Alex Sun <a314159@stanford.edu>
2021-06-29 12:18:32 -0700

    Fixed bug in the deconvolution step, and changed the shear parameters to exaggerate the shear more

--- scripts/deconvolve-shear-reconvolve.py
+++ scripts/deconvolve-shear-reconvolve.py
@@ -19,13 +19,16 @@ psf_re = 1.0
 
 pixel_scale = 0.2
 
+# shear "exaggerator"
+scalar = 5
+
 # first shear
-shear1_g1 = 0.1
-shear1_g2 = 0.2
+shear1_g1 = 0.01 
+shear1_g2 = 0.01
 
 # second shear
-shear2_g1 = -0.2
-shear2_g2 = 0.1
+shear2_g1 = 0.05
+shear2_g2 = 0.05
 
 
 # Creating galaxy from a brightness profile
@@ -47,25 +50,30 @@ convolved_galaxy = convolved.drawImage(scale=pixel_scale)
 
 
 # Deconvolving with the PSF
-inv_psf = galsim.Convolve(psf)
+inv_psf = galsim.Deconvolve(psf)
 deconvolved = galsim.Convolve(convolved, inv_psf)
 
-# Applying second shear
-sheared_2 = deconvolved.shear(g1=shear2_g1, g2=shear2_g2)
+# Applying second shears
+sheared_plus = deconvolved.shear(g1=shear2_g1, g2=shear2_g2)
+sheared_minus = deconvolved.shear(g1= -shear2_g1, g2= -shear2_g2)
 
 # Reconvolving with the PSF
-reconvolved = galsim.Convolve(sheared_2, psf)
+reconvolved_plus = galsim.Convolve(sheared_plus, psf)
+reconvolved_minus = galsim.Convolve(sheared_minus, psf)
 
 deconvolved_galaxy = deconvolved.drawImage(scale=pixel_scale)
-second_sheared_galaxy = sheared_2.drawImage(scale=pixel_scale)
-reconvolved_galaxy = reconvolved.drawImage(scale=pixel_scale)
+sheared_plus_galaxy = sheared_plus.drawImage(scale=pixel_scale)
+sheared_minus_galaxy = sheared_minus.drawImage(scale=pixel_scale)
+reconvolved_plus_galaxy = reconvolved_plus.drawImage(scale=pixel_scale)
+reconvolved_minus_galaxy = reconvolved_minus.drawImage(scale=pixel_scale)
 
 # # Writing all to a .fits file
 # galsim.fits.writeMulti([source_galaxy, sheared_galaxy, convolved_galaxy, 
 # 	deconvolved_galaxy, second_sheared_galaxy, reconvolved_galaxy], file_name='steps.fits')
 
 # Displaying with matplotlib ------------------------
-images = [source_galaxy, sheared_galaxy, convolved_galaxy, deconvolved_galaxy, second_sheared_galaxy, reconvolved_galaxy]
+images = [source_galaxy, sheared_galaxy, convolved_galaxy, deconvolved_galaxy, 
+sheared_plus_galaxy, sheared_minus_galaxy, reconvolved_plus_galaxy, reconvolved_minus_galaxy]
 
 # images = {'Source Galaxy': source_galaxy, 'Sheared Galaxy': sheared_galaxy, 'Convolved Galaxy': convolved_galaxy, 
 # 'Deconvolved Galaxy': deconvolved_galaxy, 'Second Shear': second_sheared_galaxy, 'Reconvolved Galaxy': reconvolved_galaxy}
@@ -74,7 +82,7 @@ vmax = max([np.max(image.array) for image in images])
 vmin = 0
 
 
-fig, axs = plt.subplots(2, 3, figsize=(12, 4), constrained_layout=True)
+fig, axs = plt.subplots(2, 4, figsize=(12, 4), constrained_layout=True)
 
 im = axs[0][0].imshow(source_galaxy.array, vmin=vmin, vmax=vmax)
 axs[0][0].set_title('Source Galaxy')
@@ -85,17 +93,23 @@ axs[0][1].set_title('Sheared Galaxy')
 im = axs[0][2].imshow(convolved_galaxy.array, vmin=vmin, vmax=vmax)
 axs[0][2].set_title('Sheared and Convolved Galaxy')
 
-im = axs[1][0].imshow(deconvolved_galaxy.array, vmin=vmin, vmax=vmax)
-axs[1][0].set_title('Deconvolved Galaxy')
+im = axs[0][3].imshow(deconvolved_galaxy.array, vmin=vmin, vmax=vmax)
+axs[0][3].set_title('Deconvolved Galaxy')
 
-im = axs[1][1].imshow(second_sheared_galaxy.array, vmin=vmin, vmax=vmax)
-axs[1][1].set_title('Second Shear')
+im = axs[1][0].imshow(sheared_plus_galaxy.array, vmin=vmin, vmax=vmax)
+axs[1][0].set_title('Second Shear +')
 
-im = axs[1][2].imshow(reconvolved_galaxy.array, vmin=vmin, vmax=vmax)
-axs[1][2].set_title('Reconvolved galaxy')
+im = axs[1][1].imshow(reconvolved_plus_galaxy.array, vmin=vmin, vmax=vmax)
+axs[1][1].set_title('Reconvolved Shear +')
+
+im = axs[1][2].imshow(sheared_minus_galaxy.array, vmin=vmin, vmax=vmax)
+axs[1][2].set_title('Second Shear -')
+
+im = axs[1][3].imshow(reconvolved_minus_galaxy.array, vmin=vmin, vmax=vmax)
+axs[1][3].set_title('Reconvolved Shear -')
 
 for height in range(2):
-	for width in range(3):
+	for width in range(4):
 		axs[height][width].set_xlabel('x')
 		axs[height][width].set_ylabel('y')
 
