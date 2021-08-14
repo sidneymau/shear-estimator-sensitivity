7c83d4ba81d4fcc0705ea1f57336c583ba69e6d3
Alex Sun <a314159@stanford.edu>
2021-08-14 11:20:39 -0700

    Changed the testing_generation.py script to give an example of making a plot using the mcObjects classes.

--- scripts/Metacalibration/testing_generation.py
+++ scripts/Metacalibration/testing_generation.py
@@ -1,6 +1,26 @@
 import mcObjects
 
-def main():
+
+def example_plot():
+
+    # Using a pickle file that I have already created
+    obj = mcObjects.mcSummaryObject('pickles/gaussian_data.pickle')
+
+    # slicing for pixel_scale == 0.2 only
+    obj.slice('pixel_scale', lambda scale: scale == 0.2)
+
+    # slicing for shear_estimator == 'REGAUSS' only
+    obj.slice('shear_estimator', lambda shear_est: shear_est == 'REGAUSS')
+
+    # slicing to only get rows where the calibration shear in either g1 or g2 is 0.01
+    obj.slice('cshear_dg1', lambda dg: dg == 0.01 or dg == 0.0)
+    obj.slice('cshear_dg2', lambda dg: dg == 0.01 or dg == 0.0)
+
+    obj.plot_quadratic_m(color_column='gal_psf_ratio')
+    
+    # import pdb; pdb.set_trace()
+
+def example_generation_and_plot():
 
     true_psf_profiles = {'gaussian': 0}
     shape_mes_algs = ['REGAUSS']
@@ -20,7 +40,10 @@ def main():
 
     comboObject.generate_combinations('potato', 4)
 
-    import pdb; pdb.set_trace()
+
+def main():
+
+    example_plot()
 
 if __name__ == '__main__':
     main()
