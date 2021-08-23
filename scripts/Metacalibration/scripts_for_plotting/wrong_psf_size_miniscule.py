88da2ae8fd7f100fe4c48fd47b1a515bab29b4bc
Alex Sun <a314159@stanford.edu>
2021-08-23 14:29:38 -0700

    Changed the miniscule plot to be able to plot multiple plots at the same time

--- scripts/Metacalibration/scripts_for_plotting/wrong_psf_size_miniscule.py
+++ scripts/Metacalibration/scripts_for_plotting/wrong_psf_size_miniscule.py
@@ -2,21 +2,30 @@ import mcObjects
 import sys
 import galsim
 import numpy as np
+import string
+import matplotlib.pyplot as plt
+
+
+
+
+# no change to galaxy profiles
+true_psf_profiles = {'gaussian': 0}
+shear_ests = ['REGAUSS']
+pixel_scales = [0.2]
+# calibration shears unchanged
+true_psf_fwhms = [0.7]
+# gal_psf_ratios = [2.0]
+gal_psf_ratios = [0.5]
+# offsets unchanged
+wrong_psf_fwhms = np.arange(true_psf_fwhms[0] - 0.005, true_psf_fwhms[0] + 0.006, 0.001)
+#wrong_psf_fwhms = np.arange(0.68, 0.721, 0.001)
+# wrong_psf_fwhms = [0.7]
+
+
 
 def generate():
     # DESCRIPTION OF WHAT'S BEING GENERATED
     
-
-    # no change to galaxy profiles
-    true_psf_profiles = {'gaussian': 0}
-    shear_ests = ['REGAUSS']
-    pixel_scales = [0.2]
-    # calibration shears unchanged
-    true_psf_fwhms = [0.7]
-    gal_psf_ratios = [2.0]
-    # offsets unchanged
-    wrong_psf_fwhms = np.arange(0.68, 0.721, 0.001)
-    # wrong_psf_fwhms = [0.7]
 
     comboObj = mcObjects.comboObject()
     
@@ -28,23 +37,64 @@ def generate():
     comboObj.wrong_psf_fwhms = wrong_psf_fwhms
 
     import pdb; pdb.set_trace()
-    comboObj.generate_combinations('all_gaussian_wrong_psfs_trueFWHM=0.7_miniscule_errors', 4)
+    comboObj.generate_combinations('all_gaussian_wrong_psfs_trueFWHM=0.7_ratio=0.5_miniscule_errors', 4)
 
 
 def plot():
     # DESCRIPTION OF WHAT'S BEING PLOTTED
-    obj = mcObjects.mcSummaryObject('pickles/all_gaussian_wrong_psfs_trueFWHM=0.7_miniscule_errors.pickle')
+    obj = mcObjects.mcSummaryObject('pickles/all_gaussian_wrong_psfs_trueFWHM=0.7_ratio=0.5_miniscule_errors.pickle')
     # obj.slice('deconv_psf_fwhm', lambda fwhm: fwhm == 0.7)
+
 
     wrong_psf_fwhms = np.arange(0.68, 0.721, 0.001)
     true_fwhm = 0.7
 
-    for i in range(len(wrong_psf_fwhms)):
-        # obj.slice('deconv_psf_fwhm', lambda dummy: abs(wrong_psf_fwhms[i] - true_fwhm) < 0.005)
-        obj.slice('deconv_psf_fwhm', lambda dummy: True)
-        obj.plot_quadratic_m(color_column='deconv_psf_fwhm', show=False, plotname=f"fwhm_diff={wrong_psf_fwhms[i] - true_fwhm}")
-        obj.reset()
+    obj.slice('deconv_psf_fwhm', lambda f: abs(f - true_fwhm) < 0.0055)
+    ylims, all_c1 = obj.plot_quadratic_m(color_column='deconv_psf_fwhm')
+    obj.reset()
 
+    if True:
+
+        wrong_psf_fwhm_list = []
+        c1s = []
+
+        for i in range(len(wrong_psf_fwhms)):
+            if abs(wrong_psf_fwhms[i] - true_fwhm) < 0.0055:
+                obj.slice('deconv_psf_fwhm', value=wrong_psf_fwhms[i])
+                # obj.slice('deconv_psf_fwhm', lambda dummy: True)
+                diff = wrong_psf_fwhms[i] - true_fwhm
+
+                combined_letters =  sorted(string.ascii_uppercase + string.ascii_lowercase)
+                print(combined_letters[i])
+
+                ylimits, c1 = obj.plot_quadratic_m(show=False, ylims=ylims) # plotname=f"{i}_fwhm_diff={diff:5f}")
+
+                wrong_psf_fwhm_list.append(wrong_psf_fwhms[i])
+                c1s.append(c1)
+                # obj.plot_quadratic_m(color_column='deconv_psf_fwhm', show=False, ylims=ylims, plotname=str(i))
+                obj.reset()
+
+        fig, axs = plt.subplots(1, 1, figsize=(5, 5))
+
+        print(len(wrong_psf_fwhm_list), len(c1s))
+        print(wrong_psf_fwhm_list, c1s)
+        x = np.asarray(wrong_psf_fwhm_list) - true_fwhm * np.ones(len(wrong_psf_fwhm_list))
+        y = np.asarray(c1s)
+        axs.scatter(x, y)
+
+        slope, intercept = np.polyfit(x, y, 1)
+        axs.plot(x, slope*x + intercept, label=f"slope = {slope} \n intercept = {intercept}")
+
+        axs.set_xlabel(f'delta_fwhm')
+        axs.set_ylabel(f'c (intercept of quadratic error')
+        axs.set_title('c (intercept of quadratic error) vs. delta_fwhm')
+
+        axs.axhline(color='k')
+        axs.axvline(color='k')
+
+        axs.legend()
+
+        plt.show()
 
     import pdb; pdb.set_trace()
 
